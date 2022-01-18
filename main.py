02c2b79e1b726fa7d3c12036dfb4ca9027567986
Michael Hall <mlh0079@uah.edu>
2022-01-18 12:21:29 -0600

    - Cleanup for uneeded code.

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import json
 
 import lxml.etree
 import requests
-from bs4 import BeautifulSoup
 
 
 class CmrCollection:
@@ -14,39 +13,16 @@ class CmrCollection:
         self.url = url
 
 
-class Thing:
+class CmrFetcher:
     def __init__(self):
         self.session = requests.Session()
-
-    def fetch_session(self, url, headers):
-        """
-        Establishes a session for requests.
-        """
-        return self.session.get(url, headers=headers, verify=False)
-
-    def html_request(self, url_path: str, headers: dict = None):
-        """
-        :param url_path: The base URL where the files are served
-        :param headers: Headers for the http request
-        :return: The html of the page if the fetch is successful
-        """
-        opened_url = self.fetch_session(url=url_path, headers=headers)
-        return BeautifulSoup(opened_url.text, features='html.parser')
-
-    def headers_request(self, url_path: str):
-        """
-        Performs a head request for the given url.
-        :param url_path The URL for the request
-        :return Results of the request
-        """
-        return self.session.head(url_path).headers
 
     def get_locations(self):
         collections = []
 
         csa = None
         while True:
-            headers = {'CMR-Search-After': csa, 'Accept': 'application/xml'}
+            headers = {'CMR-Search-After': csa}
             res = self.session.get('https://cmr.uat.earthdata.nasa.gov/search/collections?provider=GHRC_CLOUD',
                                    headers=headers)
             root = lxml.etree.fromstring(res.text.encode())
@@ -54,6 +30,7 @@ class Thing:
                 collections.append(
                     CmrCollection(concept_id=reference.find('id').text, url=reference.find('location').text)
                 )
+
             csa = res.headers.get('CMR-Search-After')
             if not csa:
                 break
@@ -62,7 +39,7 @@ class Thing:
 
     def get_metadata(self, locations):
         for location in locations:
-            res = self.session.get(location.url, headers={'Accept': 'application/xml'})
+            res = self.session.get(location.url)
             try:
                 root = lxml.etree.fromstring(res.text.encode())
                 location.shortname = root.findtext('ShortName')
@@ -71,25 +48,22 @@ class Thing:
                 content = json.loads(res.text)
                 location.shortname = content.get('ShortName')
                 location.version = content.get('Version')
-                pass
 
     @staticmethod
     def write_csv(collections):
         schema = ['shortname', 'version', 'concept_id', 'url']
-        with open('temp.csv', 'w', newline='') as csvfile:
-            file_writer = csv.writer(csvfile, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+        with open('collections.csv', 'w', newline='') as csv_file:
+            file_writer = csv.writer(csv_file, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
             file_writer.writerow([h for h in schema])
             for collection in collections:
                 file_writer.writerow([collection.shortname, collection.version, collection.concept_id, collection.url])
-        pass
 
 
 def main():
-    t = Thing()
+    t = CmrFetcher()
     collections = t.get_locations()
     t.get_metadata(collections)
     t.write_csv(collections)
-    pass
 
 
 if __name__ == '__main__':
